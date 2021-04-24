2b2515342b04791a444b3f978b304718a9d3b27a
mohsanabbas <mohsanabbas@cvccorp.com.br>
2021-04-24 15:30:35 -0300

    added request headers to recieve user credentionals and business unit

---
+++ src/util/decrypt/decrypt_token.go
@@ -0,0 +1,17 @@
+package decrypt
+
+import (
+	"encoding/base64"
+	"strings"
+
+	"github.com/mohsanabbas/ticketing_utils-go/logger"
+)
+
+func Decrypt(input string) []byte {
+	b64data := input[strings.IndexByte(input, '.')+1:]
+	decoded, err := base64.StdEncoding.DecodeString(b64data)
+	if err != nil {
+		logger.Error("Error : %v", err)
+	}
+	return decoded
+}
